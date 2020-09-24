8f561b4d4427fe323a76788652509ac0680e7398
DianaMartirosyan <dmart.martd@gmail.com>
2020-09-24 14:48:09 +0400

    Create function components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+function Name(props) {
+
+  return (
+    <span className="nameclass">
+      {props.text}
+    </span>
+  )
+}
+
+function Lastname(props) {
+
+  return (
+    <span className="lastnameclass">
+      {props.text}
+    </span>
+  )
+}
+
+
+function Age(props) {
+  return (
+    <p>
+      I am  {props.text} years old
+    </p>
+
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <h3>Hello, I am</h3>
+      <Name text="Ann" />
+      <Lastname text="Doe" />
+      <Age text="25" />
+      <hr />
+
+      <h3>Hello, I am</h3>
+      <Name text="John" />
+      <Lastname text="Bell" />
+      <Age text="35" />
+      <hr />
+
+      <h3>Hello, I am</h3>
+      <Name text="Alex" />
+      <Lastname text="Harris" />
+      <Age text="17" />
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/demo/demo.js
@@ -0,0 +1,12 @@
+function hello(){
+    console.log("homework")
+}
+
+export default hello;
+
+
+const name = "John";
+const lastName = "Doe";
+const age = 25
+
+export{name, lastName, age}
